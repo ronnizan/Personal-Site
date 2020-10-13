da175f53bfc65a16697cb4112c4fea956aeac39a
ronnizan <ronnizan01@gmail.com>
2020-10-13 17:19:26 +0300

    first commit

--- main.js
+++ main.js
@@ -20,13 +20,27 @@ jQuery(document).ready(function($) {
             phone: '972542356555',
             headerTitle: 'Contact Me Through WhatsApp!', 
             popupMessage: 'Hello, how can I help you?',
-            showPopup: false, 
+            showPopup: true, 
             buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', 
             headerColor: 'green',
             backgroundColor: 'green !important', 
-            position: "right"    
+            position: "left"    
           });
-       //  });
+ 
+          $(window).scroll(function () {
+            let position = window.pageYOffset;
+            $('section').each(function () {
+              let target = $(this).offset().top;
+              let id = $(this).attr('id');
+              let navigationHeight = $('#navigation').height();
+              let navLinks = $('#navigation a');
+              if (position >= target -navigationHeight) {
+                navLinks.removeClass('current');
+                $('#navigation a[href="#' + id + '"]').addClass('current');
+              }
+            });
+         });
+
     
     });
     
